319d6a364a611b3b9792192632824957913ccead
Chris Weinert <cweinert@comply365.com>
2020-07-16 12:30:54 -0500

    Initial code commit

---
+++ SafeBase64EncodeDecode/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+
+namespace SafeBase64EncodeDecode
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Encode_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Result.Text = Convert.ToBase64String(Encoding.UTF8.GetBytes(Result.Text));
+                Status.Content = "";
+            }
+            catch
+            {
+                Status.Content = "This text cannot be encoded";
+            }
+        }
+
+        private void Decode_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Result.Text = Encoding.UTF8.GetString(Convert.FromBase64String(Result.Text));
+                Status.Content = "";
+            }
+            catch
+            {
+                Status.Content = "This text cannot be decoded";
+            }
+        }
+    }
+}
